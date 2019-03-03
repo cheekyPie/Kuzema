b0d9c90de268991ea359c4e7cdc8f00dbcc82671
phil-l-brockwell <phil.l.brockwell@gmail.com>
2019-03-03 12:31:33 +0000

    Version 3

--- js/main.js
+++ js/main.js
@@ -1,10 +1,56 @@
 function setScrolls() {
 	$(".scroll-link").click(function(event) {
+		event.preventDefault();
 		const anchorId = $(this)[0].hash;
 		$("html, body").animate({ scrollTop: $(anchorId).offset().top }, "slow");
 	});
 }
 
+function setBagListItemClickEvents() {
+	$('.bag-list-item').click(function() {
+		$('.bag-list-item').removeClass('selected');
+		$(this).addClass('selected');
+		const locationParts = this.children[0].src.split('/');
+		const fileName = locationParts[locationParts.length - 1];
+		$('.bag-design-field').val(fileName);
+		const bagPreviewImage = $('.bag-preview');
+		bagPreviewImage.attr("src", ('images/' + fileName));
+	})
+}
+
+function setBagNamePreviewChangeEvent() {
+	console.log($('.bag-name-preview-field'))
+	$('.bag-name-preview-field').on('input', function() {
+		const name = this.value;
+    $('.bag-name-preview-text').text(name);
+    $('.bag-name-field').val(name);
+	});
+}
+
+function setCoverImage() {
+	const coverImageFileName = getUrlParameter('cover') || '21.jpg';
+	const cssValue = 'url(images/' + coverImageFileName + ')';
+	const heroContainer = $('#copy .top');
+	heroContainer.css('background-image', cssValue);
+}
+
+function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+      	sURLVariables = sPageURL.split('&'),
+      	sParameterName;
+
+  for (var i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+    }
+  }
+};
+
 $(document).ready(function() {
 	setScrolls();
+	setCoverImage();
+	setBagListItemClickEvents();
+	setBagNamePreviewChangeEvent();
 });
